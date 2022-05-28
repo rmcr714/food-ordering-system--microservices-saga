261d99ef85ff455198f66ecd200494fcce8a02c5
unknown <tiwari136@gmail.com>
2022-05-28 16:53:40 +0530

    Test for order service

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -11,8 +11,8 @@ public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
 
-    public Product(String name, Money price,ProductId productId) {
-        this.setId(productId);
+    public Product(ProductId productId,String name, Money price) {
+        super.setId(productId);
         this.name = name;
         this.price = price;
     }
