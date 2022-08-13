86aed6f7cd8f39691127ca12bf8395905c56be3e
unknown <tiwari136@gmail.com>
2022-08-13 16:33:44 +0530

    some changes

---
+++ common/common-application/src/main/java/com/food/ordering/system/application/handler/ErrorDTO.java
@@ -0,0 +1,20 @@
+package com.food.ordering.system.application.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorDTO {
+
+    private final String code;
+    private final String message;
+
+
+
+
+
+
+}
---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/mapper/OrderDataAccessMapper.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.order.service.dataaccess.order.mapper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDataAccessMapper {
+}
